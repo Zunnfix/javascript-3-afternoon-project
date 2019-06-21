77c30e54c945c1c8c71557507fc5cd144d95e48a
Chase Shields <cwshields2@gmail.com>
2019-06-20 19:22:36 -0500

    completed

--- destructuring.js
+++ destructuring.js
@@ -21,7 +21,7 @@ var carDetails = {
   Use object destructuring to save the property values from the object carDetails into new variables. 
 */
 
-//Code Here
+let {color, make, model, year} = carDetails;
 
 
 
@@ -34,7 +34,7 @@ var carDetails = {
 */
 
 function greeting( obj ) {
-  //Code Here
+  let {firstName, lastName, title} = obj;
   
   // Do not edit the code below.
   return 'Hello, ' + title + ' ' + firstName + ' ' + lastName + '!';
@@ -53,7 +53,10 @@ function greeting( obj ) {
   Sum up the values and return the total number.
 */
 
-//Code Here
+function totalPopulation(obj) {
+  let {utah, california, texas, arizona} = obj;
+  return utah + california + texas + arizona;
+}
 
 
 
@@ -67,8 +70,12 @@ function greeting( obj ) {
   Push these new variables to an array and return the array. 
 */
 
-//Code Here
-
+function ingredients(obj) {
+  let {carb, fat, protein} = obj;
+  let ingredients = []
+  ingredients.push(carb, fat, protein);
+  return ingredients;
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -85,8 +92,9 @@ function greeting( obj ) {
   Find the smallest number of the three and return that number.
 */
 
-//Code Here
-
+function largeNumbers( {first, second, third} ) {
+  return Math.min(first, second, third);
+}
 
 
 ////////// PROBLEM 6 //////////
@@ -97,6 +105,15 @@ function greeting( obj ) {
   Find the longest array and return that array.
 */
 
-//Code Here
-
+function numberGroups( {a, b, c} ) {
+  const arr1 = [a, b, c];
+  return arr1.reduce(function(old, new1){ 
+    if (old.length > new1.length) {
+      return old;
+    } else {
+      return new1;
+    }
+  });
+}
+// if the old value is longer than new value return the new value otherwise return value
 
